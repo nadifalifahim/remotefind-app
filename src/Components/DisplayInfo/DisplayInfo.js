64b2e99082fb08ffb2a61181633c519e913df717
nadifalifahim <nadifalifahim@gmail.com>
2021-09-27 13:22:18 +0600

    updates UI

---
+++ src/Components/DisplayInfo/DisplayInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayInfo = () => {
+  return <div></div>;
+};
+
+export default DisplayInfo;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1>RemoteFind</h1>
+      <h2>Find your remote workforce instantly</h2>
+      <p>Total Budget: $ 1,00,000</p>
+    </div>
+  );
+};
+
+export default Header;
